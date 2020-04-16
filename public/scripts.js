c13cffd71947d7208c95b3ddaa7e48432fe56a8b
Andoly <andoly.cs@hotmail.com>
2020-04-16 16:44:27 -0300

    feature: management of instructors and members

---
+++ public/scripts.js
@@ -0,0 +1,10 @@
+
+//Marcador de header ativo
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
+
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))){
+        item.classList.add("active")
+    }
+}
